7e6ddee3531987f0c8cab0b6a35355f451c651e3
Jack Desktop <nerfman317@gmail.com>
2018-09-26 15:57:02 +1000

    First Commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerController : MonoBehaviour {
+
+    public Camera cam;
+    public NavMeshAgent playerTank;
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if (Input.GetMouseButtonDown(0))
+        {
+            Ray raycast = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(raycast, out hit))
+            {
+                // MOVE OUR AGENT
+                playerTank.SetDestination(hit.point);
+            }
+        }
+	}
+}
